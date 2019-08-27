dd1d23635af59e77c292157dbc7324472cf72486
taoyibo <821869740@qq.com>
2019-08-27 14:42:13 +0800

    nothing

--- start.py
+++ start.py
@@ -8,7 +8,7 @@ from sys import argv
 if len( argv ) == 1:
     host = '192.168.11.27'
 else:
-    host = '127.0.0.1'
+    host = '127.0.0.1' #'192.168.199.179'
 server = Server(host, 8080)
 app = QApplication([])
 main_window = MainWindow(server)
